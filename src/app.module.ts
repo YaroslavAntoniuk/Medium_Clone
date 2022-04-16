b233f4a87e17b100e5487ea5020e63dc4a97bc63
YaroslavAntoniuk <yaroslav.antoniuk@binary-studio.com>
2022-04-16 13:22:30 +0300

    Added tag entity & moved config to root

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { AppController } from '@app/app.controller';
 import { AppService } from '@app/app.service';
-import ormconfig from '@app/config/ormconfig';
+import ormconfig from '@app/ormconfig';
 import { TagModule } from '@app/tag/tag.module';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
